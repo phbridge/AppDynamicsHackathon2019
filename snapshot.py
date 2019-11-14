60f3ae71728b5717d0a611aa67bb91f17d8cbbf2
simon <sihart@cisco.com>
2019-11-14 19:47:14 +0000

    snapshot

--- snapshot.py
+++ snapshot.py
@@ -4,27 +4,31 @@ import requests
 import json
 
 import requests
-import credentials
+import credentials_example
+import time
 
-CameraOne = "https://api.meraki.com/api/v0/networks/{}/cameras/{}/snapshot".format(credentials.MERAKINETID, credentials.CAMERA_1)
-CameraTwo = "https://api.meraki.com/api/v0/networks/{}/cameras/{}/snapshot".format(credentials.MERAKINETID, credentials.CAMERA_2)
+CameraOne = "https://api.meraki.com/api/v0/networks/{}/cameras/{}/snapshot".format(credentials_example.MERAKINETID, credentials_example.CAMERA_1)
+CameraTwo = "https://api.meraki.com/api/v0/networks/{}/cameras/{}/snapshot".format(credentials_example.MERAKINETID, credentials_example.CAMERA_2)
 
 def snapshot():
     headers = {
-        'X-Cisco-Meraki-API-Key': credentials.MERAKI_API,
+        'X-Cisco-Meraki-API-Key': credentials_example.MERAKI_API,
         'Content-Type': "application/x-www-form-urlencoded",
-        'cache-control': "no-cache",
         }
-
-    CA1_response = requests.request("POST", CameraOne, headers=headers)
     CA2_response = requests.request("POST", CameraTwo, headers=headers)
+    CA1_response = requests.request("POST", CameraOne, headers=headers)
 
+    #The proper list to return
     camerasnaps =[]
 
     camerasnaps.append(CA1_response.json()["url"])
     camerasnaps.append(CA2_response.json()["url"])
-    print(camerasnaps)
-#print(CA1_response.url)
-#print(CA2_response.url)
 
-    return camerasnaps
+    #Give two snapshots of the same image to Phil
+    cameraphil =[]
+    cameraphil.append(CA1_response.json()["url"])
+    cameraphil.append(CA1_response.json()["url"])
+
+    return cameraphil
+
+    #return camerasnaps
